f50f9f428a5fdcd503350e5054c871f3d08e049c
Admin <preciouspeter3173@gmail.com>
2022-11-22 23:59:19 +0100

    splash screen completed

--- app/src/main/java/SplashScreen/SplashActivity.kt
+++ app/src/main/java/SplashScreen/SplashActivity.kt
@@ -2,6 +2,7 @@ package SplashScreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dtrackerapp.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
