1a935f43c931d31ac5cd25d9e262098794c88ee8
Admin <preciouspeter3173@gmail.com>
2022-11-25 21:34:28 +0100

    recyclerview adapter added

--- app/src/main/java/com/example/dtrackerapp/ListofCountries.kt
+++ app/src/main/java/com/example/dtrackerapp/ListofCountries.kt
@@ -1,59 +1,83 @@
 package com.example.dtrackerapp
 
+import Adapter.CountryListAdapter
+import Model.CountryModel
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.dtrackerapp.databinding.CountriesBinding
+import com.example.dtrackerapp.databinding.FragmentListofCountriesBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.Collections
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ListofCountries.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ListofCountries : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+   lateinit var binding: FragmentListofCountriesBinding
+   //lateinit var countryModels: CountryModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_listof_countries, container, false)
+
+        // to get details from layout
+        binding = FragmentListofCountriesBinding.inflate(layoutInflater)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListofCountries.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListofCountries().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
+
+        val countryListAdapter = CountryListAdapter()
+
+        val countryModel: MutableList<CountryModel> = mutableListOf()
+        countryModel.add(CountryModel("Nigeria"))
+        countryModel.add(CountryModel("Ghana"))
+        countryModel.add(CountryModel("Ethiopia"))
+       countryModel.add(CountryModel("Egypt"))
+        countryModel.add(CountryModel("Kenya"))
+        countryModel.add(CountryModel("Tanzania"))
+        countryModel.add(CountryModel("South Africa"))
+        countryModel.add(CountryModel("Uganda"))
+        countryModel.add(CountryModel("Botswana"))
+        countryModel.add(CountryModel("Lesotho"))
+        countryModel.add(CountryModel("Nambibia"))
+        countryModel.add(CountryModel("Swaziland"))
+        countryModel.add(CountryModel("Somalia"))
+        countryModel.add(CountryModel("Seychelles"))
+        countryModel.add(CountryModel("Rwanda"))
+        countryModel.add(CountryModel("Mozambique"))
+        countryModel.add(CountryModel("Mauritius"))
+        countryModel.add(CountryModel("Malawi"))
+
+
+
+        countryListAdapter.differ.submitList(countryModel)
+
+                 binding.recyclerview.apply {
+
+                     adapter = countryListAdapter
+                     setHasFixedSize(true)
+                 }
+
+
+
+
+        }
+
+
+
     }
-}
